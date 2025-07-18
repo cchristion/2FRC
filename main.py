1cda5e205d2dbc8a1d1cfcd4a06bbf86bef39b28
cchristion <47944263+cchristion@users.noreply.github.com>
2025-07-18 11:34:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,109 @@
+"""Python script to display latest breachs from HIBP."""
+
+import re
+
+import pandas as pd
+import streamlit as st
+
+# ------ Page Config ------
+
+st.set_page_config(
+    page_title="JSONL Analyser",
+    page_icon=":mag:",
+    layout="wide",
+)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# ------ End of Page Config ------
+
+st.markdown("""# 2FRC: 2 File Regex compare""")
+
+pattern = st.text_area(
+    "Regex Pattern",
+    value=r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b",
+)
+pattern = re.compile(pattern, re.IGNORECASE)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    file1 = st.file_uploader("Choose a file to compare", key="f1")
+    if file1 is not None:
+        file1 = file1.read().decode("utf-8", errors="replace")
+    txt1 = st.text_area("Or paste text here", key="t1")
+    data1 = file1 or txt1
+
+with col2:
+    file2 = st.file_uploader("Choose a file to compare", key="f2")
+    if file2 is not None:
+        file2 = file2.read().decode("utf-8", errors="replace")
+    txt2 = st.text_area("Or paste text here", key="t2")
+    data2 = file2 or txt2
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if data1 and pattern:
+        data1 = pattern.findall(data1, re.IGNORECASE)
+
+        st.markdown("""### All Matchs""")
+        data1_df = pd.DataFrame(data1, columns=["Matchs"])
+        st.markdown(f"Count: {len(data1_df):,}")
+        st.dataframe(data1_df, use_container_width=True)
+
+        st.markdown("""### Unique Matchs""")
+        data1_df = pd.DataFrame(set(map(str.lower, data1)), columns=["Unique Matchs"])
+        st.markdown(f"Count: {len(data1_df):,}")
+        st.dataframe(data1_df, use_container_width=True)
+
+with col2:
+    if data2 and pattern:
+        data2 = pattern.findall(data2, re.IGNORECASE)
+
+        st.markdown("""### All Matchs""")
+        data2_df = pd.DataFrame(data2, columns=["Matchs"])
+        st.markdown(f"Count: {len(data2_df):,}")
+        st.dataframe(data2_df, use_container_width=True)
+
+        st.markdown("""### Unique Matchs""")
+        data2_df = pd.DataFrame(set(map(str.lower, data2)), columns=["Unique Matchs"])
+        st.markdown(f"Count: {len(data2_df):,}")
+        st.dataframe(data2_df, use_container_width=True)
+
+
+if data1 and data2:
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("""### Matches that are not in the other""")
+        diff1_df = set(map(str.lower, data1)) - set(map(str.lower, data2))
+        diff1_df = pd.DataFrame(diff1_df, columns=["Matchs"])
+        st.markdown(f"Count: {len(diff1_df):,}")
+        st.dataframe(diff1_df, use_container_width=True)
+
+    with col2:
+        st.markdown("""### Matches that are not in the other""")
+        diff2_df = set(map(str.lower, data2)) - set(map(str.lower, data1))
+        diff2_df = pd.DataFrame(diff2_df, columns=["Matchs"])
+        st.markdown(f"Count: {len(diff2_df):,}")
+        st.dataframe(diff2_df, use_container_width=True)
+
+    st.markdown("""### Common Matchs""")
+    data_all_df = set(map(str.lower, data1)) & set(map(str.lower, data2))
+    st.markdown(f"Count: {len(data_all_df):,}")
+    data_all_df = pd.DataFrame(set(data1) & set(data2), columns=["Common Matchs"])
+    st.dataframe(data_all_df, use_container_width=True)
+
+st.markdown(
+    """---
+by **Christion C** | [LinkedIn](https://www.linkedin.com/in/cchristion) | [GitHub](https://github.com/cchristion)
+""",
+    help=None,
+)
